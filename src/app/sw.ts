75bc16449b3fe63c221e9ced86f7b2ef671a9208
NAVEEN K <naveenkkn03@gmail.com>
2024-09-25 17:28:22 +0530

    push function added

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -13,6 +13,29 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || "/icon.png",
+      badge: "/badge.png",
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: "2",
+      },
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("Notification click received.");
+  event.notification.close();
+  // event.waitUntil(clients.openWindow("<https://your-website.com>"));
+});
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
